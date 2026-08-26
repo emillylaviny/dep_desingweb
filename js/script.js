3775b28d8f5bb174f033731621afbb33eed1db3d
emillylaviny <emillylaviny034@gmail.com>
2026-08-26 16:15:41 -0300

    sintaxe 1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-alert("oi do interno"); 
+var nasc = parseInt(prompt("Digite seu ano de nascimento: "));
+let  nome = prompt("Digite seu nome: ");
+const viva = true
+let altura = parseFloat(prompt("Digite sua altura em metros:"));
 
-var num = parseFloat(prompt("Digite um número: "))
-if (num == 0){
-    alert(`${num} é zero`);
-}else if (num %2 != 0){
-    alert(`${num} é impar`);
+let idade;
+
+if (viva){
+    idade = 2026 - nasc;
+    alert(`${nome} você tem ${idade} anos.`);
 }else{
-    alert(`${num} é par`);
+    alert(`Você está morta.`);
 }
 
 alert("Fim");
