484d2b871d261d640850238b9c8aa8327892ec81
emillylaviny <emillylaviny034@gmail.com>
2026-08-26 15:59:51 -0300

    add sintaxe

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ var num = parseFloat(prompt("Digite um número: "))
 if (num == 0){
     alert(`${num} é zero`);
 }else if (num %2 != 0){
-    alert(`${num} é impar`)
+    alert(`${num} é impar`);
 }else{
-    alert(`${num} é par`)
+    alert(`${num} é par`);
 }
 
-alert("Fim")
+alert("Fim");
