c9bd7ebb709e3b4a0e1138a9ad625f91c54eb1b5
emillylaviny <emillylaviny034@gmail.com>
2026-08-26 15:59:05 -0300

    sintaxe

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("oi do interno");
+alert("oi do interno"); 
+
+var num = parseFloat(prompt("Digite um número: "))
+if (num == 0){
+    alert(`${num} é zero`);
+}else if (num %2 != 0){
+    alert(`${num} é impar`)
+}else{
+    alert(`${num} é par`)
+}
+
+alert("Fim")
