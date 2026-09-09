e2479237db020e718d5ecbdd1a0992b306b608dc
emilly laviny <emillylaviny034@gmail.com>
2026-09-09 14:30:25 -0300

    criando op

---
+++ js/script4.js
@@ -0,0 +1,16 @@
+let dia = prompt("Digite o dia da semana sendo 1-domingo e 7-sábado");
+if(dia ==1){
+    alert("Hoje é domingo");
+}else if (dia == 2){
+    alert("Hoje é segunda");
+}else if (dia == 3){
+    alert("Hoje é terça");
+}else if (dia == 4){
+    alert("Hoje é quarta");
+}else if (dia == 5){
+    alert("Hoje é quinta");
+}else if (dia == 6){
+    alert("Hoje é sexta");
+}else if (dia == 7){
+    alert("Hoje é sábado");
+}
