dde2c0ca92ca639988faec679def6fbcf36de0c9
emillylaviny <emillylaviny034@gmail.com>
2026-09-16 15:24:47 -0300

    eventos

--- js/script3.js
+++ js/script3.js
@@ -19,3 +19,30 @@ btn_a.addEventListener("click", function(e){
     e.preventDefault();
     saudacao_a();
 })
+
+const btn_b = document.getElementById("btn_b");
+btn_b.addEventListener("click", function(e){
+    e.preventDefault();
+    let nome = prompt("Digite seu nome:");
+    saudacao_b(nome);
+})
+
+const btn_cp = document.getElementById("btn_cp");
+cp.addEventListener("click", function(e){
+    e.preventDefault();
+    nome = prompt("Digite seu nome:");
+    saudacao_c(nome);
+})
+
+const btn_cn = document.getElementById("btn_cn");
+c.addEventListener("click", function(e){
+    e.preventDefault();
+    let nome = prompt("Digite seu nome:");
+    saudacao_c(nome);
+})
+
+const btn_dn = document.getElementById("btn_dn");
+btn_dn.addEventListener("click", function(e){
+    e.preventDefault();
+    alert(saudacao_d());
+})
